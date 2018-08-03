7f93204684943b3466cd5690995b7321879b7fd6
fdinc01 <fdinc01@fdinc01demac-mini.local>
2018-08-03 11:23:18 +0800

    添加设置界面

---
+++ PlayImageTool/subs/XDSettingCtrl.h
@@ -0,0 +1,13 @@
+//
+//  XDSettingCtrl.h
+//  PlayImageTool
+//
+//  Created by fdinc01 on 2018/8/3.
+//  Copyright © 2018年 fdinc01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XDSettingCtrl : UIViewController
+
+@end
